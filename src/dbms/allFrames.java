850a9b4f48cef7900a5397471e509c1dc63a6e61
Dheeraj Gupta S D <sdheerajgupta99@gmail.com>
2020-06-29 13:40:52 +0530

    DBMS-Module1

---
+++ src/dbms/allFrames.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dbms;
+
+/**
+ *
+ * @author me
+ */
+public interface allFrames {
+    public static Startpage2 sp2 = new Startpage2();
+    public static decompositon d = new decompositon();
+    public static outputFrame of = new outputFrame();
+}
